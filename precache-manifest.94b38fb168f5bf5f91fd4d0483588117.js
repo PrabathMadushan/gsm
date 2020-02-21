aeba1946e3b62d2a24744317777163c4c9e6910f
Prabath Madhushan <prabathmadushan5@gmail.com>
2020-02-22 02:02:45 +0530

    Updates

---
+++ precache-manifest.94b38fb168f5bf5f91fd4d0483588117.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a658e6b4e52b836679149f731be49cc",
+    "url": "/gsm/index.html"
+  },
+  {
+    "revision": "984da3f92e105eb1c5d6",
+    "url": "/gsm/static/css/main.a555a4d1.chunk.css"
+  },
+  {
+    "revision": "6c6e82f0e78d13fbc886",
+    "url": "/gsm/static/js/2.a2b69ed3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/gsm/static/js/2.a2b69ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "984da3f92e105eb1c5d6",
+    "url": "/gsm/static/js/main.6eea0190.chunk.js"
+  },
+  {
+    "revision": "44a50e1f9c34b27faec0",
+    "url": "/gsm/static/js/runtime-main.423fb4e0.js"
+  }
+]);
