371cd6d65c71a0a51fe9513a809ba280a4097c14
PrabathMadushan <prabathmadushan5@gmail.com>
2020-03-24 22:35:59 +0530

    commit new

---
+++ src/components/dialog/dialog.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './dialog.scss'
+
+const Dialog = ({children}) => {
+
+    return <div className='com-dialog-main-wrapper'>
+            
+        </div>
+};
+
+export default Dialog;
